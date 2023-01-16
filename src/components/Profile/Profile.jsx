695a7a098d51a4691a2ca26f4636edf5c9b5ba51
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-01-16 16:28:52 +0300

    project

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import MyPosts from './MyPosts/MyPosts.jsx';
+import p from './Profile.module.css';
+
+const Profile = () => {
+     
+    return (
+    <profile className={p.profile}>
+    <div >
+      <Profile />
+    <div>
+    <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBsbCAQtQimp5yI0Zx4vyR_FzPLUVzkdjDBN0N4_LAUo59inNQrSp6-Iz7qrfAXBENLGI&usqp=CAU' alt='' />
+    </div>
+    <div>
+      ava + discription
+    </div>
+   <MyPosts />
+  </div>
+  </profile>
+  )
+}
+
+export default Profile;
