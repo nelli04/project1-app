aa4a5189072ab7f8a0c203f4a95d96f1f011ce69
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-01-16 15:36:54 +0300

    project

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -13,7 +13,7 @@ const Profile = () => {
     <div>
       ava + discription
     </div>
-   
+   <MyPosts />
   </div>
   </profile>
   )
--- src/index.js
+++ src/index.js
@@ -3,17 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react';
-import { BrowserRouter } from 'react-router-dom';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-    <Provider>
+    
+    
     <App />
-    </Provider>
-    </BrowserRouter>
+    
+    
   </React.StrictMode>
 );
 
