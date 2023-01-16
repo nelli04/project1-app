8abe7514383c99d593aa92484876aa142cf980fc
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-01-16 15:28:05 +0300

    project

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
+    <Provider>
     <App />
+    </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
