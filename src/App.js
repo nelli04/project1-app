e5d536496e8c8ba10de30e3cc2c139a81be69306
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-01-16 16:19:50 +0300

    project

--- src/App.js
+++ src/App.js
@@ -3,9 +3,8 @@ import './App.css';
 import { Routes, Route, BrowserRouter} from "react-router-dom"
 import Dialogs from './components/Dialogs/Dialogs';
 import Header from './components/Header/Header';
-
 import Navbar from './components/Navbar/Navbar';
-import Profile from './components/Profile/Profile';
+
 
 
 const App = (props)  => {
@@ -14,13 +13,12 @@ const App = (props)  => {
     <div className = 'app-wrapper'>
       <Header />
       <Navbar /> 
-      <Profile /> 
+       
       
       <div className = 'app-wrapper-content'>
         <Routes>
         <Route path = '/dialogs' element={<Dialogs/>}/>
-        <Route path = '/profile' element = {<Profile/>}/>
-        
+      
         </Routes>
       </div>
     </div>
