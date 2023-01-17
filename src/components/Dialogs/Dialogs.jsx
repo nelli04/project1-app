0264f286c42d0838e792ba71e2a735350fe27aff
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-01-17 13:15:55 +0300

    project

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,38 +3,40 @@ import { NavLink } from 'react-router-dom';
 import d from './Dialogs.module.css';
 
 const DialogItem = (props) => {
-    
+
+    let path = "/dialogs/" + props.id;
+
     return (
         <div className = {d.dialog + ' ' + d.active} >
-                   <NavLink to = {path}>
+                   <NavLink to={path}>
                     {props.name}
-                   </NavLink> 
+                   </NavLink>
         </div>
     )
 }
 
 const MessageItem = (props) => {
     return (
-        <NavLink to = {d.dialog}>
+        <div className = {d.dialog}>
             {props.message}
-        </NavLink>
+        </div>
     )
 }
 
  
 const Dialogs = () => {
           let dialogsData = [
-       {id: Jeck,  name: 'Jeck'},
-       {id: Nick, name: 'Nick'},
-       {id: Aleksandr, name: 'Aleksandr' }, 
-       {id: Mike, name: 'Mike'}
+       {id: 1,  name: 'Jeck'},
+       {id: 2, name: 'Nick'},
+       {id: 3, name: 'Aleksandr' }, 
+       {id: 4, name: 'Mike'}
  ] 
     
 let messagesData = [
-       {id: Jeck,  message: 'Hi'},
-       {id: Nick, message: 'Hello'},
-       {id: Aleksandr, message: 'Hey' }, 
-       {id: Mike, message: 'Yo'}
+       {id: 1,  message: 'Hi'},
+       {id: 2, message: 'Hello'},
+       {id: 3, message: 'Hey' }, 
+       {id: 4, message: 'Yo'}
  ]
    
     return (
