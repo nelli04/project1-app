377ac89decb6cf4f406f0127705e689b8205c7ae
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-01-17 15:37:12 +0300

    project

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -25,13 +25,22 @@ const MessageItem = (props) => {
 
  
 const Dialogs = () => {
-          let dialogsData = [
+
+
+    let dialogsData = [
        {id: 1,  name: 'Jeck'},
        {id: 2, name: 'Nick'},
        {id: 3, name: 'Aleksandr' }, 
        {id: 4, name: 'Mike'}
  ] 
     
+ let dialogsElements = [
+                <DialogItem name = {dialogsData[0].name} id = {dialogsData[0].id} />,
+                <DialogItem name = {dialogsData[1].name} id = {dialogsData[1].id} />,
+                <DialogItem name = {dialogsData[2].name} id = {dialogsData[2].id} />,
+                <DialogItem name = {dialogsData[3].name} id = {dialogsData[3].id} />
+ ]
+
 let messagesData = [
        {id: 1,  message: 'Hi'},
        {id: 2, message: 'Hello'},
