d43d8894d68bf3196a9aff79965e2925152ab9c9
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-01-17 13:21:35 +0300

    project

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import c from './MyPosts.module.css';
 import Post from './Post/Post';
 
-const MyPosts = () => {
+const MyPosts = (props) => {
+
+  let postData = [
+    {id: 1,  message: 'Hi', likesCount: 10},
+    {id: 2, message: 'Hello', likesCount: 10}
+]
+
     return (
     <div>
-
       <h3>My Posts</h3>
-
       <div>
 
         <div>
@@ -21,8 +25,8 @@ const MyPosts = () => {
       </div>
 
       <div className = {c.posts}>
-        <Post message = 'Hello'/>
-        <Post message = 'Hi' />
+        <Post message = {postData[0].message} likesCount = {postData[0].likesCount} />
+        <Post message = {postData[1].message} likesCount = {postData[1].likesCount}/>
       </div>
 
     </div>
