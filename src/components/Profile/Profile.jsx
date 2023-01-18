02e541a83419991e96a0b697613193bdb249f517
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-01-18 19:55:49 +0300

    f

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,15 +2,28 @@ import React from 'react';
 import MyPosts from './MyPosts/MyPosts';
 import p from './Profile.module.css';
 import ProfileItems from './ProfileItems/ProfileItems';
+import Post from './MyPosts/Post/Post';
+import c from './MyPosts/MyPosts.module.css'
 
 const Profile = () => {
+
+  let postData = [
+    {id: 1,  message: 'Hi', likesCount: 10},
+    {id: 2, message: 'Hello', likesCount: 10}
+]
+
+let postElement = postData.map(p => <Post message = {p.message} likesCount = {p.likesCount} />) 
      
     return (
     <profile className = {p.profile}>
     <div >
       <ProfileItems />
-      <MyPosts />
+      <MyPosts posts = {postElement}/>
   </div>
+
+    <div className = {c.posts}>
+        {postElement}
+      </div>
   </profile>
   )
 }
