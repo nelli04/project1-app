7f30a422a3817a1ac0273e4b403b95c781752bf6
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-01-18 20:12:46 +0300

    project

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import c from './MyPosts.module.css'
+import postData from './../../../App'
+
 const MyPosts = (props) => {
+
+  let postElement = postData.map(p => <Post message = {p.message} likesCount = {p.likesCount} />) 
 
     return (
     <div>
@@ -15,7 +20,9 @@ const MyPosts = (props) => {
         </div>
 
       </div>
-
+      <div className = {c.posts}>
+        {postElement}
+      </div>
     </div>
   )
 }
