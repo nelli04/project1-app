b0d00acbc685f6202f1e315e2f6a7b917afe852c
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-01-19 15:15:42 +0300

    project

--- src/index.js
+++ src/index.js
@@ -9,10 +9,24 @@ let postData = [
   {id: 2, message: 'Hello', likesCount: 10}
 ]
 
+let dialogsData = [
+  {id: 1,  name: 'Jeck'},
+  {id: 2, name: 'Nick'},
+  {id: 3, name: 'Aleksandr' }, 
+  {id: 4, name: 'Mike'}
+] 
+
+let messagesData =  [
+  {id: 1,  message: 'Hi'},
+  {id: 2, message: 'Hello'},
+  {id: 3, message: 'Hey' }, 
+  {id: 4, message: 'Yo'}     
+]  
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App postData = {postData}/>
+    <App postData = {postData} dialogsData = {dialogsData} messagesData = {messagesData}/>
   </React.StrictMode>
 );
 
