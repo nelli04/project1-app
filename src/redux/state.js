a2921628284afdce3b4285cb0c7d76b1584f8c9d
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-01-19 16:22:39 +0300

    project

---
+++ src/redux/state.js
@@ -0,0 +1,20 @@
+  let state = {
+    postData: [
+        {id: 1,  message: 'Hi', likesCount: 10},
+        {id: 2, message: 'Hello', likesCount: 10}
+    ], 
+    dialogsData: [
+        {id: 1,  name: 'Jeck'},
+        {id: 2, name: 'Nick'},
+        {id: 3, name: 'Aleksandr' }, 
+        {id: 4, name: 'Mike'}
+      ],
+    messagesData: [
+        {id: 1,  message: 'Hi'},
+        {id: 2, message: 'Hello'},
+        {id: 3, message: 'Hey' }, 
+        {id: 4, message: 'Yo'}     
+      ]  
+  }
+
+  export default state;
