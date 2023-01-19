0802308483588bcf1169e394ad1dbd80524f2bfd
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-01-19 13:42:30 +0300

    project

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,15 +3,18 @@ import MyPosts from './MyPosts/MyPosts';
 import p from './Profile.module.css';
 import ProfileItems from './ProfileItems/ProfileItems';
 
-const Profile = () => {
+const Profile = (props) => {
 
-  
+  let postData = [
+    {id: 1,  message: 'Hi', likesCount: 10},
+    {id: 2, message: 'Hello', likesCount: 10}
+]
      
     return (
     <profile className = {p.profile}>
     <div >
       <ProfileItems />
-      <MyPosts />
+      <MyPosts postData = {postData}/>
   </div>
 
   </profile>
