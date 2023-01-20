410b1c6157f4c66a92d688e49912ab2f60245a5a
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-01-20 15:46:40 +0300

    project

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -7,13 +7,19 @@ const Dialogs = (props) => {
 
      let dialogsElements = props.state.dialogsData.map(d => <DialogItem name = {d.name} id = {d.id}/>)
      let messagesElements = props.state.messagesData.map(m => <MessageItem message = {m.message}/>)
+     let newMessages = React.createRef();
+     let textMessages = () => {
+        let text = newMessages.current.value;
+        alert(text)
+     }
 
     return (
         <div className = {d.dialogs}>
             <div className = {d.dialogsItems}>
                 {dialogsElements}                
             </div>
-
+            <textarea ref = {newMessages}></textarea>
+            <button className = 'button' onClick = {textMessages}>Отправить</button>
             <div className = {d.messagesItems}>
                 {messagesElements}
             </div>
