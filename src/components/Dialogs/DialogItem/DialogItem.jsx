5c7f519bbbfa8bdfcdc99d6ac309d1f601b077ff
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-01-20 17:05:24 +0300

    project

--- src/components/Dialogs/DialogItem/DialogItem.jsx
+++ src/components/Dialogs/DialogItem/DialogItem.jsx
@@ -3,7 +3,9 @@ import { NavLink } from 'react-router-dom';
 import d from './../Dialogs.module.css';
 
 const DialogItem = (props) => {
+
     let path = "/dialogs/" + props.id;
+    
     return (
         <div className = {d.dialog + ' ' + d.active} >
                    <NavLink to = {path}>
