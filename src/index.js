2fa55ee4b9b2bdb41065dd966aacbf134d1655e3
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-01-20 10:34:24 +0300

    project

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import state from './redux/state';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App state = {state.dialogsData}/>
+    <App state = {state}/>
   </React.StrictMode>
 );
 
