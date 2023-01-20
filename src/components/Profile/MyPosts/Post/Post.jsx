2d03d28c6437dc157b5dd87d4a3c4c7955994190
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-01-20 17:05:40 +0300

    project

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -5,11 +5,14 @@ const Post = (props) => {
 
     return (      
       <div className={s.item}>
+        
         <img src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT42STwjfKg1x3AmsXTwpXHV6DmN-ZfGoJp1A&usqp=CAU' alt = ''/>
         {props.message}
+
         <div>
         <span>Like</span>
         </div>
+
       </div>
   )
 }
