abc2d29c1c2fa9e588321150bb72340e98ebfb93
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-01-22 13:36:50 +0300

    project

--- src/index.js
+++ src/index.js
@@ -2,9 +2,20 @@ import './index.css';
 import { rerenderEntireTree } from './render/render';
 import reportWebVitals from './reportWebVitals';
 import state from './redux/state';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import {addPost, updateNewPostText} from './../redux/state';
 
-
-
+const root = ReactDOM.createRoot(document.getElementById('root'));
+export let rerenderEntireTree = (state) => {    
+  root.render(
+    <React.StrictMode>
+      <App state = {state} addPost = {addPost} updateNewPostText = {updateNewPostText}/>
+    </React.StrictMode>
+  );
+  }
 rerenderEntireTree(state);
 
 // If you want to start measuring performance in your app, pass a function
