19a6373346dbe03763fae59586e2ac4dfb66a547
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-01-23 17:29:32 +0300

    project

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -8,12 +8,12 @@ const MyPosts = (props) => {
   let newPostElement = React.createRef();
 
   let addPost = () => {
-    props.dispatch((type: 'ADD-POST'))
+    props.dispatch({type: 'ADD-POST'})
   }
   
   let onPostChange = () => {
     let text = newPostElement.current.value;
-    props.dispatch((type: 'UPDATE-NEW-POST-TEXT'));
+    props.dispatch({type: 'UPDATE-NEW-POST-TEXT'});
   }
 
 
