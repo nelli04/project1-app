4a6dde4cde8f23653dfe24718b95be889fd02c12
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-01-25 14:30:22 +0300

    project

--- src/redux/state.js
+++ src/redux/state.js
@@ -53,4 +53,16 @@ let store = {
   }
 }
 
+export const addPostActionCreator = () => {
+    return {
+        type: 'ADD-POST'
+    }
+}
+
+export const updateNewPostActionCreator = (text) => {
+    return {
+        type: 'UPDATE-NEW-POST-TEXT', newText: text
+    }
+}
+
 export default store;
