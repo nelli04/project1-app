d447484400782f5753524ac505964c0407eb42f6
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-01-26 16:46:34 +0300

    project

--- src/redux/messagesReducer.js
+++ src/redux/messagesReducer.js
@@ -1,12 +1,15 @@
 const UPDATE_NEW_MESSAGE_TEXT = 'UPDATE-NEW-MESSAGE-TEXT'
 const SEND_MESSAGE = 'SEND_MESSAGE'
 const messagesReducer = (state, action) => {
-     if (action.type === UPDATE_NEW_MESSAGE_TEXT) {
-        state.newMessageText = action.messageBody;
-    } else if (action.type === SEND_MESSAGE) {
-        let messageBody = state.newMessageText
-        state.newMessageText = '';
-        state.messagesData.push({id: 6, message: messageBody})
+    switch (action.type) {
+        case UPDATE_NEW_MESSAGE_TEXT:
+            state.newMessageText = action.messageBody;
+            break;
+        case SEND_MESSAGE:
+            let messageBody = state.newMessageText
+            state.newMessageText = '';
+            state.messagesData.push({id: 6, message: messageBody})
+            break;
     }
     return state;
 }
