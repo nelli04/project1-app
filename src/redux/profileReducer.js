f99af52412a2d1a42a003ff598732363194be7d9
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-01-26 16:46:51 +0300

    project

--- src/redux/profileReducer.js
+++ src/redux/profileReducer.js
@@ -1,16 +1,19 @@
 const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 const profileReducer = (state, action) => {
-    if (action.type === ADD_POST) {
-        let newPost = {
-            id: 5,
-            message: this._state.profile.newPostText,
-            likesCount: 10
-        };
-        state.postData.push(newPost);
-        state.newPostText = '';
-    } else if (action.type === UPDATE_NEW_POST_TEXT) {
-         state.profile.newPostText = action.newText;
+    switch (action.type) {
+        case ADD_POST:
+            let newPost = {
+                id: 5,
+                message: state.newPostText,
+                likesCount: 10
+            };
+            state.postData.push(newPost);
+            state.newPostText = '';
+            break;
+        case UPDATE_NEW_POST_TEXT:
+            state.profile.newPostText = action.newText;
+            break;
     }
     return state;
 }
