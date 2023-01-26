b937e3dc068368d295da0923f5cae57e065525a2
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-01-26 16:52:45 +0300

    project

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import c from './MyPosts.module.css'
 import Post from './Post/Post'
-import {addPostActionCreator, updateNewPostActionCreator} from "../../../redux/state";
+import {addPostActionCreator, updateNewPostActionCreator} from "../../../redux/profileReducer";
 
 const MyPosts = (props) => {
  
