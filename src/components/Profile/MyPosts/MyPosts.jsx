fea88a5ed983410fa60238d8eb64903c3e1d6809
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-01-27 22:34:28 +0300

    project

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -7,12 +7,10 @@ const MyPosts = (props) => {
   let newPostElement = React.createRef();
   let addPost = () => {
     props.addPost();
-    //props.dispatch(addPostActionCreator())
   }
   let onPostChange = () => {
     let text = newPostElement.current.value;
     props.updateNewPostText(text);
-    //let action = updateNewPostActionCreator(text);
   }
     return (
     <div >
