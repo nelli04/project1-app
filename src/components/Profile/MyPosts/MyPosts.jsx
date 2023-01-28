aea5749052e1e7c2221777b62e7c3fbc487ff39f
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-01-28 21:32:18 +0300

    project

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,37 +3,37 @@ import c from './MyPosts.module.css'
 import Post from './Post/Post'
 
 const MyPosts = (props) => {
-  let postElement = props.postData.map(p => <Post message = {p.message} likesCount = {p.likesCount} />)
-  let newPostElement = React.createRef();
-  let addPost = () => {
-    props.addPost();
-  }
-  let onPostChange = () => {
-    let text = newPostElement.current.value;
-    props.updateNewPostText(text);
-  }
+    let postElement = props.postData.map(p => <Post message={p.message} likesCount={p.likesCount}/>)
+    let newPostElement = React.createRef();
+    let addPost = () => {
+        props.addPost();
+    }
+    let onPostChange = () => {
+        let text = newPostElement.current.value;
+        props.updateNewPostText(text);
+    }
     return (
-    <div >
-      <div className = { c.MyPosts }>
-      <h3 >My Posts</h3>
-      </div>
-    <div>
-        <div className = {c.text}>
-          <textarea onChange = { onPostChange }
-                    ref = { newPostElement }
-                    value = {props.newPostText}/>
-        </div>
         <div>
-          <button className  = { c.button}
-                  onClick = { addPost }>Отправить
-          </button>
+            <div className={c.MyPosts}>
+                <h3>My Posts</h3>
+            </div>
+            <div>
+                <div className={c.text}>
+          <textarea onChange={onPostChange}
+                    ref={newPostElement}
+                    value={props.newPostText}/>
+                </div>
+                <div>
+                    <button className={c.button}
+                            onClick={addPost}>Отправить
+                    </button>
+                </div>
+            </div>
+            <div className={c.posts}>
+                {postElement}
+            </div>
         </div>
-      </div>
-      <div className = {c.posts}>
-           {postElement}
-      </div>
-    </div>
-  )
+    )
 }
 
 export default MyPosts;
