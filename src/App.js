ea167d586d8d4920f225a51af98feebd8e283838
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-01-28 21:32:37 +0300

    project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Routes, Route, BrowserRouter} from "react-router-dom"
+import {Routes, Route, BrowserRouter} from "react-router-dom"
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Music from './components/Music/Music';
@@ -9,25 +9,25 @@ import Settings from './components/Settings/Settings';
 import News from './components/News/News';
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 
-const App = (props)  => {
-  debugger;
-  return (
-    <BrowserRouter>
-    <div className = 'app-wrapper'>
-      <Header />
-      <Navbar /> 
-      <div className = 'app-wrapper-content'>
-        <Routes>
-        <Route path = '/dialogs' element = {<DialogsContainer store = {props.store} />}/>
-        <Route path = '/profile' element = {<Profile store = {props.store} />}/>
-        <Route path = '/header' element = {<Header/>}/>
-        <Route path = '/music' element = {<Music/>}/>
-        <Route path = '/news' element = {<News/>}/>
-        <Route path = '/settings' element = {<Settings/>}/>
-        </Routes>
-      </div>
-    </div>
-    </BrowserRouter>
-  )
+const App = (props) => {
+    debugger;
+    return (
+        <BrowserRouter>
+            <div className='app-wrapper'>
+                <Header/>
+                <Navbar/>
+                <div className='app-wrapper-content'>
+                    <Routes>
+                        <Route path='/dialogs' element={<DialogsContainer/>}/>
+                        <Route path='/profile' element={<Profile/>}/>
+                        <Route path='/header' element={<Header/>}/>
+                        <Route path='/music' element={<Music/>}/>
+                        <Route path='/news' element={<News/>}/>
+                        <Route path='/settings' element={<Settings/>}/>
+                    </Routes>
+                </div>
+            </div>
+        </BrowserRouter>
+    )
 }
 export default App;
