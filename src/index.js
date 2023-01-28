15129fabe11ce8eb91337521124c4618bacb0359
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-01-28 21:31:20 +0300

    project

--- src/index.js
+++ src/index.js
@@ -5,15 +5,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import StoreContext from "./storeContext";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-export let rerenderEntireTree = (state) => {    
-  root.render(
-    <React.StrictMode>
-      <App state = {state} dispatch = {store.dispatch.bind(store)} store = {store} />
-    </React.StrictMode>
-  );
-  }
+export let rerenderEntireTree = (state) => {
+    root.render(
+        <React.StrictMode>
+            <StoreContext.Provider value={store}>
+                <App/>
+            </StoreContext.Provider>
+        </React.StrictMode>
+    );
+}
 rerenderEntireTree(store.getState());
 
 store.subscribe(() => {
