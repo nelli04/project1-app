70022f8804bd06074c0bb4cabca77273207d9a6f
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-01-28 15:53:39 +0300

    project

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,7 +3,7 @@ import c from './MyPosts.module.css'
 import Post from './Post/Post'
 
 const MyPosts = (props) => {
-  let postElement = props.postData.map(p => <Post message = {p.message} likesCount = {p.likesCount} />) 
+  let postElement = props.postData.map(p => <Post message = {p.message} likesCount = {p.likesCount} />)
   let newPostElement = React.createRef();
   let addPost = () => {
     props.addPost();
