48e1e2dbad0cafbe963ea0ed351800632f8b1ce6
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-01-29 20:28:58 +0300

    project

--- src/components/Dialogs/DialogsContainer.jsx
+++ src/components/Dialogs/DialogsContainer.jsx
@@ -2,25 +2,21 @@ import React from 'react';
 import {sendMessageCreator, updateNewMessageBodyCreator} from "../../redux/messagesReducer";
 import Dialogs from "./Dialogs";
 import {connect} from "react-redux";
+
 let mapObjState = (state) => {
     return {
         messages: state.messages
     }
 }
-
 let mapObjDispatch = (dispatch) => {
     return {
-        update: () => {
-           dispatch(sendMessageCreator())
+        send: () => {
+            dispatch(sendMessageCreator())
         },
-        send: (body) => {
+        update: (body) => {
             dispatch(updateNewMessageBodyCreator(body))
         }
     }
 }
-
-
 const DialogsContainer = connect(mapObjState, mapObjDispatch)(Dialogs);
-
-
 export default DialogsContainer;
