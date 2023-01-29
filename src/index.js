9f8bf350e73c3d239353be2ca25ebdf4080b147f
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-01-29 13:31:38 +0300

    project

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {Provider} from "./storeContext";
+import {Provider} from "react-redux";
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 export let rerenderEntireTree = (state) => {
