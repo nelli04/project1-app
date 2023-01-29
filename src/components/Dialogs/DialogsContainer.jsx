3d8a06d45ba8947a4fbc1525b0c3a66ffc9ab7a2
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-01-29 19:18:48 +0300

    project

--- src/components/Dialogs/DialogsContainer.jsx
+++ src/components/Dialogs/DialogsContainer.jsx
@@ -2,33 +2,6 @@ import React from 'react';
 import {sendMessageCreator, updateNewMessageBodyCreator} from "../../redux/messagesReducer";
 import Dialogs from "./Dialogs";
 import {connect} from "react-redux";
-
-/*const DialogsContainer = () => {
-    /!*let stateOne = props.store.getState().messages;
-    let onSendMessageClick = () => {
-        props.store.dispatch(sendMessageCreator())
-    }
-    let onNewMessageChange = (body) => {
-        props.store.dispatch(updateNewMessageBodyCreator(body))
-    }*!/
-    return (
-        <StoreContext.Consumer>
-            {(store) => {
-                let stateOne = store.getState().messages;
-                let onSendMessageClick = () => {
-                    store.dispatch(sendMessageCreator())
-                }
-                let onNewMessageChange = (body) => {
-                    store.dispatch(updateNewMessageBodyCreator(body))
-                }
-                return (<Dialogs update={onNewMessageChange}
-                                 send={onSendMessageClick}
-                                 messages={stateOne}/>)
-            }
-            }
-        </StoreContext.Consumer>
-    )
-}*/
 let mapObjState = (state) => {
     return {
         messages: state.messages
