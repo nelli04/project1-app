97c2e221ce8f6213630885a11c63ff4d9fe734df
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-01-30 16:41:46 +0300

    project

--- src/components/Profile/MyPosts/MyPostsContainer.jsx
+++ src/components/Profile/MyPosts/MyPostsContainer.jsx
@@ -3,38 +3,6 @@ import {addPostActionCreator, updateNewPostActionCreator} from "../../../redux/p
 import MyPosts from "./MyPosts";
 import {connect} from "react-redux";
 
-/*const MyPostsContainer = () => {
-    debugger;
-    //let state = props.store.getState();
-    /!*let addPost = () => {
-        props.store.dispatch(addPostActionCreator())
-    }
-    let onPostChange = (text) => {
-        let action = updateNewPostActionCreator(text);
-        props.store.dispatch(action);
-    }*!/
-    return (
-        <StoreContext.Consumer>
-            {
-                (store) => {
-                    let state = store.getState();
-                    let addPost = () => {
-                        store.dispatch(addPostActionCreator())
-                    }
-                    let onPostChange = (text) => {
-                        let action = updateNewPostActionCreator(text);
-                        store.dispatch(action);
-                    }
-                    return (<MyPosts updateNewPostText={onPostChange}
-                                     addPost={addPost}
-                                     postData={state.profile.postData}
-                                     newPostText={state.profile.newPostText}/>)
-                }
-            }
-        </StoreContext.Consumer>
-    )
-}*/
-
 let mapObjState = (state) => {
     return {
         postData: state.profile.postData,
