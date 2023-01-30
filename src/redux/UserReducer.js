aa03c901c868eb19832c7ca8188e8007b510b7bf
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-01-30 23:33:39 +0300

    project

--- src/redux/UserReducer.js
+++ src/redux/UserReducer.js
@@ -2,11 +2,20 @@ const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 
 let initialState = {
-    postData: [
-        {id: 1, message: 'Hi', likesCount: 10},
-        {id: 2, message: 'Hello', likesCount: 10}
-    ],
-    newPostText: ''
+    users: [
+        {
+            id: 1, name: 'Nelli', status: 'good', location: {
+                city: 'Msk',
+                country: 'Russia'
+            }
+        },
+        {
+            id: 2, name: 'Vik', status: 'good', location: {
+                city: 'Msk',
+                country: 'Russia'
+            }
+        }
+    ]
 }
 const UserReducer = (state = initialState, action) => {
     switch (action.type) {
