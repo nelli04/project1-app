ddb9cd0ec1823e0cba815eae5a26d7bbe1f6d7db
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-01-30 23:55:06 +0300

    project

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,10 +3,11 @@ import h from './Header.module.css';
 
 const Header = () => {
     return (
-    <header className = {h.header}>
-    <img src='https://marketplace.canva.com/EAE72d1muoo/1/0/1600w/canva-professional-initial-letter-typography-logo-Fu3NHNsjlB4.jpg' alt=''/>
-  </header>
-  )
+        <header className={h.header}>
+            <img
+                src='https://marketplace.canva.com/EAE72d1muoo/1/0/1600w/canva-professional-initial-letter-typography-logo-Fu3NHNsjlB4.jpg'
+                alt=''/>
+        </header>
+    )
 }
-
 export default Header;
