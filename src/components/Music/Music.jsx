01cf960b3ad67802459f30f64b2bfb5d2e74d452
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-01-30 23:55:27 +0300

    project

--- src/components/Music/Music.jsx
+++ src/components/Music/Music.jsx
@@ -3,10 +3,9 @@ import m from './Music.module.css'
 
 const Music = () => {
     return (
-        <music className = {m.music}> 
+        <music className={m.music}>
 
         </music>
-   )         
+    )
 }
-
 export default Music;
