beefc656333e768063fdc19e07c4f1b166cbb36c
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-01-30 23:55:34 +0300

    project

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,28 +1,26 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 import n from './Navbar.module.css';
 
 const Navbar = () => {
     return (
-    <nav className = {n.nav}>
-    <div className = {`${n.item}${n.active}`}>
-      <NavLink to = '/profile' activeClassName = {n.activeLink}>Profile</NavLink>
-    </div>
-    <div className = {`${n.item}${n.active}`}>
-      <NavLink to = '/dialogs' activeClassName = {n.activeLink}>Messages</NavLink>
-    </div>
-    <div className = {`${n.item}${n.active}`}>
-      <NavLink to = 'news' activeClassName = {n.activeLink}>News</NavLink>
-    </div>
-    <div className = {`${n.item}${n.active}`}>
-      <NavLink to = 'music' activeClassName = {n.activeLink}>Music</NavLink>
-    </div>
-    <div className = {`${n.item}${n.active}`}>
-      <NavLink to = 'setting'activeClassName = {n.activeLink}>Settings</NavLink>
-    </div>
-  </nav>
+        <nav className={n.nav}>
+            <div className={`${n.item}${n.active}`}>
+                <NavLink to='/profile' activeClassName={n.activeLink}>Profile</NavLink>
+            </div>
+            <div className={`${n.item}${n.active}`}>
+                <NavLink to='/dialogs' activeClassName={n.activeLink}>Messages</NavLink>
+            </div>
+            <div className={`${n.item}${n.active}`}>
+                <NavLink to='news' activeClassName={n.activeLink}>News</NavLink>
+            </div>
+            <div className={`${n.item}${n.active}`}>
+                <NavLink to='music' activeClassName={n.activeLink}>Music</NavLink>
+            </div>
+            <div className={`${n.item}${n.active}`}>
+                <NavLink to='setting' activeClassName={n.activeLink}>Settings</NavLink>
+            </div>
+        </nav>
     )
 }
-
-
 export default Navbar;
