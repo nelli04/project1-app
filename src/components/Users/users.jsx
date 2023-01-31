36232fb871f5a75bc231afbacfa795a014c0f933
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-01-31 15:12:19 +0300

    project

--- src/components/Users/users.jsx
+++ src/components/Users/users.jsx
@@ -1,9 +1,29 @@
 import React from "react";
+import u from './user.module.css'
 
-export const Users = () => {
-    return (
-        <div>
-            USEEER
-        </div>
-    )
+export const Users = (props) => {
+    return <div>
+        {
+            props.user.map(el => <div key={el.id}>
+            <span>
+                <div className={u.photo}>
+                    <img src={el.photo}/>
+                </div>
+                <div>
+                    <button>Follow</button>
+                </div>
+            </span>
+                <span>
+                <span>
+                    <div>{el.name}</div>
+                    <div>{el.status}</div>
+                </span>
+                <span>
+                    <div>{el.location.country}</div>
+                    <div>{el.location.city}</div>
+                </span>
+            </span>
+            </div>)
+        }
+    </div>
 }
