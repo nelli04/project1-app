56006fb316b5175633ca18aa6faf76ef1d08d1ee
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-01-31 12:34:42 +0300

    project

--- src/redux/ReduxStore/reduxStore.js
+++ src/redux/ReduxStore/reduxStore.js
@@ -1,10 +1,12 @@
 import {combineReducers, createStore} from "redux";
 import profileReducer from "../profileReducer";
 import messagesReducer from "../messagesReducer";
+import userReducer from "../UserReducer";
 
 let reducersStore = combineReducers({
     profile: profileReducer,
     messages: messagesReducer,
+    user: userReducer
 });
 
 let store = createStore(reducersStore);
