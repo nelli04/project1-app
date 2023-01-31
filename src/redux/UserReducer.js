13a6e67f3ed9a995f0c4e48150e333f20fa2c194
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-01-31 12:19:04 +0300

    project

--- src/redux/UserReducer.js
+++ src/redux/UserReducer.js
@@ -1,26 +1,27 @@
 const FOLLOW = 'ADD-POST';
 const UNFOLLOW = 'UPDATE-NEW-POST-TEXT';
+const SET_USER = 'SET_USER';
 
 let initialState = {
     users: [
-        {
-            id: 1, follow: false, name: 'Nelli', status: 'good', location: {
-                city: 'Msk',
-                country: 'Russia'
-            }
-        },
-        {
-            id: 2, follow: true, name: 'Vik', status: 'good', location: {
-                city: 'Msk',
-                country: 'Russia'
-            }
-        }
+        /* {
+             id: 1, follow: false, name: 'Nelli', status: 'good', location: {
+                 city: 'Msk',
+                 country: 'Russia'
+             }
+         },
+         {
+             id: 2, follow: true, name: 'Vik', status: 'good', location: {
+                 city: 'Msk',
+                 country: 'Russia'
+             }
+         }*/
     ]
 }
 const UserReducer = (state = initialState, action) => {
     switch (action.type) {
         case FOLLOW:
-            let stateOne = {
+            return {
                 ...state,
                 users: state.users.map(el => {
                     if (el.id === action.userId) {
@@ -30,13 +31,28 @@ const UserReducer = (state = initialState, action) => {
                 })
             }
         case UNFOLLOW:
+            return {
+                ...state,
+                users: state.users.map(el => {
+                    if (el.id === action.userId) {
+                        return {...el, follow: false}
+                    }
+                    return el;
+                })
+            }
 
-        default
-        :
+        case SET_USER:
+            return {
+                ...state,
+                user: {...state.user, ...action.user}
+            }
+
+        default:
             return state;
 
     }
 }
 export const followAC = (userId) => ({type: FOLLOW, userId})
 export const unFollowAC = (userId) => ({type: UNFOLLOW, userId})
+export const setUserAC = (user) => ({type: SET_USER, user})
 export default UserReducer;
