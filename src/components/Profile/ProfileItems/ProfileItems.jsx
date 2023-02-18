de1a38147a3d5bcfd022977dcb7eebea1cdb3c92
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-02-18 15:31:23 +0300

    project

--- src/components/Profile/ProfileItems/ProfileItems.jsx
+++ src/components/Profile/ProfileItems/ProfileItems.jsx
@@ -3,18 +3,19 @@ import pro from './ProfileItems.module.css';
 
 const ProfileItems = () => {
     return (
-    <div >
-    <div className = {pro.container}>
-    <img className = {pro.img} src = 'https://img.freepik.com/premium-vector/doodle-face-avatar-for-social-media-line-art-people-face-isolated-on-white-background_559729-359.jpg' alt='' />
-    <div className = {pro.overlay}>
-      @username
-    </div>
-    </div>
-         <div className = {pro.ava}>
-      @username
-    </div>
-  </div>
-  )
+        <div>
+            <div className={pro.container}>
+                <img className={pro.img} src='https://img.freepik.com/premium-vector/doodle-face-avatar-for-social-media-line-art-people-face-isolated-on-white-background_559729-359.jpg'
+                     alt=''/>
+                <div className={pro.overlay}>
+                    @username
+                </div>
+            </div>
+            <div className={pro.ava}>
+                @username
+            </div>
+        </div>
+    )
 }
 
 export default ProfileItems;
